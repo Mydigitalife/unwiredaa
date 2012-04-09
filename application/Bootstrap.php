991a737594a8eda89d6cf04d76db8b8aff9646c1
Boyan Krastev <bkrastev@web-teh.net>
2012-04-09 18:36:26 +0300

    Use ini for timezone

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -42,10 +42,10 @@ class Unwired_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
      * Get rid of the annoying message "It is not safe to rely on systems timezone..."
      *
      */
-    protected function _initTimeZone()
+  /*  protected function _initTimeZone()
     {
         date_default_timezone_set('Europe/Sofia');
-    }
+    } */
 
     /**
      * Init event broker
